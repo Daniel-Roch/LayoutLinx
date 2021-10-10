c03797b263fdec1b248f7246f07c8ab138b0ba18
DanielRoch4 <daniel.rocha@dclick.com.br>
2021-10-10 18:13:28 -0300

    Fazendo um novo fetch para adicionar mais cards ao clicar no botão: 'ainda mais produtos aqui!', também finalizando o cass da página principal e do footer

--- JS/main.js
+++ JS/main.js
@@ -5,17 +5,30 @@ fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?pag
 
 
 function criaCards(card){
-    console.log(card)
     for(let i = 0; i<card.length; i++){
         $('#cards').append(`
         <div class="card">
-            <img src="${card[i].image}"
-            <p>${card[i].name}</p>
-            <p>${card[i].description}</p>
-            <p>De: R$${card[i].oldPrice}</p>
-            <p class="cardPreco">Por: R$${card[i].price}</p>
-            <p>ou 2x de R$${card[i].price / 2}</p>
+            <img class="cardImg" src="${card[i].image}"/>
+            <p class="cardName">${card[i].name}</p>
+            <p class="cardDesc">${card[i].description}</p>
+            <p class="cardOldPrice">De: R$${card[i].oldPrice}</p>
+            <p class="cardNewPrice">Por: R$${card[i].price}</p>
+            <p class="cardPrice2x">ou 2x de R$${card[i].price / 2}</p>
+            <button class="cardBtn">Comprar</button>
         </div>
         `)
     }
 }
+
+
+//generating more items: 
+const btnMore = document.querySelector("#moreBtn")
+let click = 1
+
+btnMore.addEventListener('click',(e)=>{
+    e.preventDefault()
+    click++
+    fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${click}`)
+    .then(res => res.json())
+    .then(json => criaCards(json.products))
+})
